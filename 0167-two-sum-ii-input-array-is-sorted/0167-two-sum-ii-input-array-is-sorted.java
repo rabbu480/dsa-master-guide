9f18e387de0ef647a2634d0e578f34f596fc6a8d
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-07-07 09:30:17 +0530

    Time: 2 ms (95.82%), Space: 48.4 MB (42.16%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
 
+        // O(n),O(n)
         // Map<Integer,Integer> numMap= new HashMap<>();
         // for(int i =0 ; i< numbers.length ; i++){
         //     if(numMap.containsKey(target-numbers[i])){
@@ -14,21 +15,36 @@ class Solution {
         // }
         // return null;
         
-        // Via two pointer:
-    
-    
-        for(int i = 0; i < numbers.length ; i++){
-            int j = i+1;
-            while(j < numbers.length){
-                if(numbers[i] + numbers[j]==target){
-                    int[] result= new int[2];
-                    result[0]= i+1;
-                    result[1]=j+1;
-                    return result;
-                }
-                j++;
+        // Via two pointer: O(n2)
+        // for(int i = 0; i < numbers.length ; i++){
+        //     int j = i+1;
+        //     while(j < numbers.length){
+        //         if(numbers[i] + numbers[j]==target){
+        //             int[] result= new int[2];
+        //             result[0]= i+1;
+        //             result[1]=j+1;
+        //             return result;
+        //         }
+        //         j++;
+        //     }
+        // }
+        // return null;
+
+        // O(n) O(1)
+        int right = numbers.length-1;
+        int left = 0; 
+        while(left < right){
+            int sum =numbers[left]+numbers[right];
+            if(sum < target){
+                left++;
+            } else if(sum > target ){
+                right --;
+            } else {
+                return new int[]{left + 1, right + 1};
             }
+
         }
+
         return null;
 
     }
