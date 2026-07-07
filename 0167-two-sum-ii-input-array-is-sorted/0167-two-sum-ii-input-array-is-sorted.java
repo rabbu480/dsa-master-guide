7249877b493543a5f5b584745375aa74a486c45a
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-07-07 09:14:44 +0530

    Time: 411 ms (5.11%), Space: 48.5 MB (24.44%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,18 +1,35 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
 
-        Map<Integer,Integer> numMap= new HashMap<>();
-        for(int i =0 ; i< numbers.length ; i++){
-            if(numMap.containsKey(target-numbers[i])){
-                int[] result= new int[2];
-                result[0]=numMap.get(target-numbers[i])+1;
-                result[1]=i+1;
-                return result; 
-                // retun new Integer { numMap.containsKey(target-numbers[i]) ,i  } ;
+        // Map<Integer,Integer> numMap= new HashMap<>();
+        // for(int i =0 ; i< numbers.length ; i++){
+        //     if(numMap.containsKey(target-numbers[i])){
+        //         int[] result= new int[2];
+        //         result[0]=numMap.get(target-numbers[i])+1;
+        //         result[1]=i+1;
+        //         return result; 
+        //         // retun new Integer { numMap.containsKey(target-numbers[i]) ,i  } ;
+        //     }
+        //     numMap.put(numbers[i],i);
+        // }
+        // return null;
+        
+        // Via two pointer:
+    
+    
+        for(int i = 0; i < numbers.length ; i++){
+            int j = i+1;
+            while(j < numbers.length){
+                if(numbers[i] + numbers[j]==target){
+                    int[] result= new int[2];
+                    result[0]= i+1;
+                    result[1]=j+1;
+                    return result;
+                }
+                j++;
             }
-            numMap.put(numbers[i],i);
         }
         return null;
-        
+
     }
 }
