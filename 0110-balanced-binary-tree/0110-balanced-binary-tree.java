75c6a579cb78e8df5860c15049bef0bc677fa2e5
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-07-08 19:03:34 +0530

    Time: 0 ms (100%), Space: 45.4 MB (76.74%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,24 +14,56 @@
  * }
  */
 class Solution {
+    // this is O(n2) avoid.
+    // public boolean isBalanced(TreeNode root) {
+    //     if(root == null ) return true;
+    //     int left=maxDepth(root.left);
+    //     int right=maxDepth(root.right);
+    //     // System.out.println("left::"+left +" right ::" + right );
+    //     // current node balanced?
+    //     boolean currentBalanced =
+    //         Math.abs(left - right) <= 1;
+    //     return currentBalanced && isBalanced(root.left)
+    //         && isBalanced(root.right);
+    // }
+
+    // int maxDepth(TreeNode root){
+    //     if(root == null ) return 0;
+    //     int left = maxDepth(root.left);
+    //     int right = maxDepth(root.right);
+    //     // System.out.println("left::"+left +" right ::" + right );
+    //     return  Math.max(left,right)+1;       
+    // }
+
+    boolean isBalnced=true;
     public boolean isBalanced(TreeNode root) {
-        if(root == null ) return true;
-        int left=maxDepth(root.left);
-        int right=maxDepth(root.right);
-        // System.out.println("left::"+left +" right ::" + right );
-        // current node balanced?
-        boolean currentBalanced =
-            Math.abs(left - right) <= 1;
-        return currentBalanced && isBalanced(root.left)
-            && isBalanced(root.right);
+
+        // 1
+        // /\
+        // 2 2
+        // /  \
+        // 3  3
+        // /   \
+        // 4    4
+
+        if(root == null){
+            return true;
+        }
+
+        maxDepth( root);
+        return isBalnced;
     }
 
-    int maxDepth(TreeNode root){
-        if(root == null ) return 0;
+    public int maxDepth(TreeNode root){
+        if(root == null) return 0;
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
-        // System.out.println("left::"+left +" right ::" + right );
-        return  Math.max(left,right)+1;       
+        // each node from root checkif this balanced or not 
+        if(Math.abs(left-right) > 1 ) {
+            isBalnced= false;
+            return -1;
+        }        
+        return 1+ Math.max(left,right);
     }
 
 }
