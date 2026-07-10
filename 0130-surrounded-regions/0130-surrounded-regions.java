2dfe39cafd6e7b88ec7756630dce6449cc2e003a
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-07-10 17:24:03 +0530

    Time: 33 ms (6.22%), Space: 47.3 MB (95.89%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,70 @@
+class Solution {
+    public void solve(char[][] board) {
+
+        // get the protected in board
+        for(int r =0 ; r< board.length;r++) {
+            if(board[r][0] == 'O'){
+                dfs(board,r,0);
+            }
+            if(board[r][board[0].length-1] == 'O'){
+                dfs(board,r,board[0].length-1);
+            }
+        }
+        for(int c =0 ;c< board[0].length;c++) {
+            if(board[0][c] == 'O'){
+                dfs(board,0,c);
+            }
+            if(board[board.length-1][c] == 'O'){
+                dfs(board,board.length-1,c) ;
+            }
+        }
+        // make everything else except protected once in board.
+        // System.out.println("board >>>" + Arrays.toString(board[0] ) );
+        // System.out.println("board >>>" + Arrays.toString(board[board[0].length-1] ) );
+        for(int r =0 ;r< board.length;r++) {
+            for(int c =0 ; c< board[0].length;c++) {
+
+                if(board[r][c] == 'O'){
+                    board[r][c] = 'X' ;
+                }
+                // if(board[r][c] == 'T'){
+                //     board[r][c] = 'O' ;
+                // }  
+
+            }
+        }
+        System.out.println("board >>>" + Arrays.toString(board ) );
+        for(int r =0 ;r< board.length;r++) {
+            for(int c =0 ; c< board[0].length;c++) {
+
+                // if(board[r][c] == 'O'){
+                //     board[r][c] = 'X' ;
+                // }
+                if(board[r][c] == 'T'){
+                    board[r][c] = 'O' ;
+                }  
+
+            }
+        }
+
+        // return board;
+    }
+
+    public void dfs(char[][] board,int r, int c){
+        if(r<0 || c < 0|| r>=board.length || c>= board[0].length ){
+            return;
+        }
+        System.out.println("board>>ooo" + board[r][c]  );
+        // mark as protected 
+        if(board[r][c] == 'O'){
+            System.out.println("board>>ooo" + board[r][c]  );
+            board[r][c] = 'T';
+            
+            dfs(board,r,c+1); 
+            dfs(board,r,c-1);
+            dfs(board,r-1,c);
+            dfs(board,r+1,c);
+            
+        }
+    }
+}
