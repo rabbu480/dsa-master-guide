b5c2aabd5d0b7143156a7bba1354afc12890c2b8
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-07-10 15:47:25 +0530

    Time: 0 ms (100%), Space: 46.6 MB (78.93%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -4,8 +4,8 @@ class Solution {
         // for(int r =0; r< image.length; r++){
         //     for(int c =0; c< image[r].length; c++){
                 // if(r == sr && c == sc){
-                    int val = image[sr][sc];
-                    dfs( image,sr,sc,val, color, new boolean[ image.length][image[0].length]) ;
+                    int originalColor = image[sr][sc];
+                    dfs( image,sr,sc,originalColor, color, new boolean[ image.length][image[0].length]) ;
                 // }
         //     }
         // }
@@ -13,19 +13,19 @@ class Solution {
         
     }
 
-    public void dfs(int[][] image,int r,int c, int val,int color,boolean[][] visted){
+    public void dfs(int[][] image,int r,int c, int originalColor,int color,boolean[][] visted){
         if(r < 0 || c< 0||  r>= image.length || c >= image[0].length){
             return ;
         }
-        if(image[r][c] != val ) return ;
+        if(image[r][c] != originalColor ) return ;
 
-        if(image[r][c] == val && image[r][c] != color){
+        if(image[r][c] != color){
             image[r][c] = color;
             // visted[r][c]=true; 
-            dfs(image,r,c+1,val,color,visted);
-            dfs(image,r,c-1,val,color,visted);
-            dfs(image,r+1,c,val,color,visted);
-            dfs(image,r-1,c,val,color,visted);
+            dfs(image,r,c+1,originalColor,color,visted);
+            dfs(image,r,c-1,originalColor,color,visted);
+            dfs(image,r+1,c,originalColor,color,visted);
+            dfs(image,r-1,c,originalColor,color,visted);
         }
 
 
