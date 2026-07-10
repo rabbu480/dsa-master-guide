627bb3578f3abf0a1821fe93e37cbd9c6dc30992
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-07-10 17:32:01 +0530

    Time: 35 ms (6.22%), Space: 47.2 MB (98.76%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,24 +1,26 @@
 class Solution {
     public void solve(char[][] board) {
 
+        int rows = board.length;
+        int cols = board[0].length;
         // get the protected in board
-        for(int r =0 ; r< board.length;r++) {
+        for(int r =0 ; r< rows;r++) {
             // row on column first and last i.e board[0].length-1
-            if(board[r][0] == 'O'){
+            // if(board[r][0] == 'O'){
                 dfs(board,r,0);
-            }
-            if(board[r][board[0].length-1] == 'O'){
-                dfs(board,r,board[0].length-1);
-            }
+            // }
+            // if(board[r][c] == 'O'){
+                dfs(board,r,cols-1);
+            // }
         }
-        for(int c =0 ;c< board[0].length;c++) {
+        for(int c =0 ;c< cols;c++) {
             // column on first and last row  board[0].length-1
-            if(board[0][c] == 'O'){
+            // if(board[0][c] == 'O'){
                 dfs(board,0,c);
-            }
-            if(board[board.length-1][c] == 'O'){
-                dfs(board,board.length-1,c) ;
-            }
+            // }
+            // if(board[rows][c] == 'O'){
+                dfs(board,rows-1,c) ;
+            // }
         }
         // make everything else except protected once in board.
         // System.out.println("board >>>" + Arrays.toString(board[0] ) );
@@ -29,25 +31,25 @@ class Solution {
                 if(board[r][c] == 'O'){
                     board[r][c] = 'X' ;
                 }
-                // if(board[r][c] == 'T'){
-                //     board[r][c] = 'O' ;
-                // }  
-
-            }
-        }
-        System.out.println("board >>>" + Arrays.toString(board ) );
-        for(int r =0 ;r< board.length;r++) {
-            for(int c =0 ; c< board[0].length;c++) {
-
-                // if(board[r][c] == 'O'){
-                //     board[r][c] = 'X' ;
-                // }
-                if(board[r][c] == 'T'){
+                else if(board[r][c] == 'T'){
                     board[r][c] = 'O' ;
                 }  
 
             }
         }
+        // System.out.println("board >>>" + Arrays.toString(board ) );
+        // for(int r =0 ;r< board.length;r++) {
+        //     for(int c =0 ; c< board[0].length;c++) {
+
+        //         // if(board[r][c] == 'O'){
+        //         //     board[r][c] = 'X' ;
+        //         // }
+        //         if(board[r][c] == 'T'){
+        //             board[r][c] = 'O' ;
+        //         }  
+
+        //     }
+        // }
 
         // return board;
     }
