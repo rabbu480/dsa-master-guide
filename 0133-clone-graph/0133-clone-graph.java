8dd14ed42a6526c6fe30431652d4efacfe96ea63
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-07-11 17:35:00 +0530

    Time: 41 ms (7.15%), Space: 45.4 MB (7.93%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,56 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    Map<Node,Node> clonedMap= new HashMap();
+    public Node cloneGraph(Node node) {
+
+        return dfs(node);
+    }
+ 
+
+    public Node dfs(Node node){
+        // check current node 
+        // make current negbor as visted 
+        // get all neigbors;
+        if(node == null) return null;
+
+        if(clonedMap.containsKey(node)){
+            return clonedMap.get(node);
+        }
+
+        Node nodeCopy = new Node(node.val);
+        clonedMap.put(node,nodeCopy);
+        // System.out.println("clonedMap:: " + clonedMap);
+        if(node.neighbors != null ){
+            System.out.println("node.val:: " +node.val);
+            ArrayList al = new ArrayList();
+            for(Node nodez : node.neighbors){
+                // here dfs return each negbors negbors
+                // neighbors.add(nodeCopy);
+                Node neighbor = dfs(nodez);
+                nodeCopy.neighbors.add(neighbor);
+                System.out.println("nodez.val::: " +nodeCopy);
+            }
+        }
+        return nodeCopy;
+
+    }
+}
