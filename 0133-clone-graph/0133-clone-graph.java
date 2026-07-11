7db9c5bc6f79f84193ea822966d15b13d01cfd4c
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-07-11 21:04:00 +0530

    Time: 61 ms (7.15%), Space: 52.8 MB (7.93%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -34,12 +34,14 @@ class Solution {
         if(clonedMap.containsKey(node)){
             return clonedMap.get(node);
         }
-
+        
         Node nodeCopy = new Node(node.val);
         clonedMap.put(node,nodeCopy);
+        printMap(clonedMap);
         // System.out.println("clonedMap:: " + clonedMap);
+        // printNode(">> nodeCopy >>>",nodeCopy);
         if(node.neighbors != null ){
-            System.out.println("node.val:: " +node.val);
+            // System.out.println("node.val:: " +node.val);
             for(Node nodez : node.neighbors){
                 // here dfs return each negbors negbors
                 // neighbors.add(nodeCopy);
@@ -47,11 +49,67 @@ class Solution {
                 //1 ==> 
                 // 2
                 // get related and add to it so now here 2 add relation 1
+                // printNode(">>> neighbor >>>",neighbor);
                 nodeCopy.neighbors.add(neighbor);
-                System.out.println("nodez.val::: " +nodeCopy);
+                // System.out.println("nodez.val::: " +nodeCopy);
             }
         }
+        // printNode(">>> after >>>",nodeCopy);
+
         return nodeCopy;
 
+    }
+
+    private void printMap(Map<Node, Node> clonedMap) {
+
+        System.out.println("\n========== Clone Map ==========");
+
+        for (Map.Entry<Node, Node> entry : clonedMap.entrySet()) {
+
+            System.out.println(
+                "Original " + entry.getKey().val +
+                " --> " +
+                nodeToString(entry.getValue())
+            );
+        }
+
+        System.out.println("================================\n");
+    }
+
+    private String nodeToString(Node node) {
+
+        if (node == null) {
+            return "null";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Clone ")
+        .append(node.val)
+        .append(" -> [ ");
+
+        for (Node neighbor : node.neighbors) {
+            sb.append(neighbor.val).append(" ");
+        }
+
+        sb.append("]");
+
+        return sb.toString();
+    }
+
+    private void printNode(String prefix,Node node) {
+
+        if (node == null) {
+            System.out.println("null");
+            return;
+        }
+
+        System.out.print(prefix+ " Node " + node.val + " -> [ ");
+
+        for (Node neighbor : node.neighbors) {
+            System.out.print(neighbor.val + " ");
+        }
+
+        System.out.println("]");
     }
 }
