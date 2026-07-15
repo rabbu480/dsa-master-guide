56bccfbb9687470ff50e5b1b77db8fd88c94eeac
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-07-16 00:53:06 +0530

    Time: 15 ms (55.21%), Space: 47.8 MB (19.72%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,37 +1,26 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        
-        // as per constraints o(n) we need aim for 
-        // so as far as i know get map of frequency conts
-        // use heaps for min heap to get only topelevel element i mean top k frequenct
-        HashMap<Integer,Integer> freqMap= new HashMap<>();
-        for(int num: nums){
-            // freqMap.computeIfAbasent(num,1).add(freqMap.get(num)+1);
+
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        // Get Map with freqcies.
+        for(int num : nums){
             freqMap.put(num,freqMap.getOrDefault(num,0)+1);
         }
-        // now map has all the values 
-        Queue<Integer> freqElements = new PriorityQueue((a,b) ->freqMap.get(a) - freqMap.get(b) );
-        //
-        // ArrayList<Integer> freqElements = new ArrayList();
-        for(int freqKey: freqMap.keySet() ){
-            // if(freqMap.get(freqKey) >= k){
-                freqElements.offer(freqKey);
-                if(freqElements.size() > k ){
-                    freqElements.poll();
-                }
-            // }
+        PriorityQueue<Integer> pq=new PriorityQueue((a,b) ->  freqMap.get(a) - freqMap.get(b));
+        for(int key: freqMap.keySet()){
+            pq.offer(key);
+            if(pq.size() > k){
+                pq.poll();
+            }
         }
-        int[] freqElementResult= new int[freqElements.size()];
-        int freqElementIndex =0;
-        // for(int freqElement:freqElements){
-        //     freqElementResult[freqElementIndex]=freqElement;
-        //     freqElementIndex++;
-        // }
-        while(!freqElements.isEmpty()){
-            freqElementResult[freqElementIndex]=freqElements.poll();
-            freqElementIndex++;
+        int[] result= new int[pq.size()];
+        int i=0;
+        while(!pq.isEmpty()){
+            result[i]=pq.poll();
+            i++;
         }
-        return freqElementResult;
+    return result;
+
         
     }
 }
