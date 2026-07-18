335867ad8edf97a984ccad8cfd70b17d4395c814
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-07-18 08:47:02 +0530

    Time: 0 ms (100%), Space: 43.6 MB (82.42%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -9,22 +9,20 @@ class Solution {
                 tergetIndex= mid;
                 return mid;
             }
-            
+            // [4,5,6,7,8,9,0,1,2]
+            // check if Left is sorted
             if( nums[left] <= nums[mid]  ){
-                // sorted 
-                // 4,5,6,7,0,1,2 target=5
-                // 
+                // sorted mid is 8 taget 9
+                // Left is not sorted i.e right is sorted
+                // check if the target in the range then use the half else discard & check next half.
                 if( nums[left] <= target && target < nums[mid]  ){
                     right=mid-1;
                 } else {
                     left= mid+1;
                 }
-            } 
-
-            if( nums[right] >= nums[mid]){
-                // sorted 
-                // 6,7,0,1,2,3,4 target 3
-                // 6,7,8,9,0,1
+            } else {
+                // Left is not sorted i.e irght is sorted
+                // check if the target in the range then use the half else discard go right.
                 if(  nums[mid] < target  && target <= nums[right] ){
                     left=mid+1;
                 } else {
