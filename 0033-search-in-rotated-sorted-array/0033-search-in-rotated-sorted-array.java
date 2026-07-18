0d0d9c43b14079f5ce9370784dae04105a4e6472
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-07-18 08:37:38 +0530

    Time: 0 ms (100%), Space: 43.7 MB (82.42%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -14,7 +14,7 @@ class Solution {
                 // sorted 
                 // 4,5,6,7,0,1,2 target=5
                 // 
-                if( nums[left] <= target && target <= nums[mid]  ){
+                if( nums[left] <= target && target < nums[mid]  ){
                     right=mid-1;
                 } else {
                     left= mid+1;
@@ -25,7 +25,7 @@ class Solution {
                 // sorted 
                 // 6,7,0,1,2,3,4 target 3
                 // 6,7,8,9,0,1
-                if(  nums[mid] <= target  && target <= nums[right] ){
+                if(  nums[mid] < target  && target <= nums[right] ){
                     left=mid+1;
                 } else {
                     right= mid-1;
