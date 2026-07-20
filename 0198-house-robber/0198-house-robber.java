21beb086ff1c5bb10d5140ab8481bd1427c12085
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-07-20 14:20:20 +0530

    Time: 0 ms (100%), Space: 42.5 MB (88.26%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int rob(int[] nums) {
+        int[] dp= new int[nums.length];
+        Arrays.fill(dp,-1); 
+
+        return maxAmount(0,nums , dp);
+    }
+    // State (start from house 0) , Choice (1 skip 2 )  ==> Rob this house OR Skip this house .
+    // recurrence, Base codition, recusion 
+    // Memorization dp[]  fill -1
+
+    int maxAmount(int i, int[] nums , int[] dp){ 
+        if(i>= nums.length) return 0;
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        // either start 1 ,start 2
+        dp[i]= Math.max(maxAmount(i+1,nums,dp),nums[i]+maxAmount(i+2,nums,dp));
+        return dp[i];
+    }
+
+
+
+
+}
