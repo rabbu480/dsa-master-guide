461c3f6ed2d8cee0813ad09670386e8eb78fc680
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-07-20 15:23:03 +0530

    Time: 6 ms (0.77%), Space: 42.8 MB (42.01%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int rob(int[] nums) {
+        int lastIndex=nums.length;
+        int[] dp= new int[nums.length];
+        if (nums.length == 1) return nums[0];
+        // 0,1,2 => 6 0,2  1,
+        Arrays.fill(dp,-1);
+        System.out.println("dp::>" +Arrays.toString(dp));
+        int robStartAt0=robMaxAmnt(0,lastIndex-2,nums,dp);
+        System.out.println("dp::>>" +Arrays.toString(dp));
+        Arrays.fill(dp,-1);
+        System.out.println("dp::<" +Arrays.toString(dp));
+        int robStartAt1=robMaxAmnt(1,lastIndex-1,nums,dp);
+        System.out.println("dp::<<" +Arrays.toString(dp));
+        return Math.max(robStartAt0,robStartAt1);
+
+    }
+
+    // State 0 i, choice rob or skip  
+    // recurrence,basecase, recurssion 
+    // memorization 
+    int robMaxAmnt(int i,int robTill,int[] nums, int[] dp ){
+
+        if(i > robTill) return 0;
+
+        if(dp[i] != -1){
+            return dp[i];
+        }
+
+        int skip=robMaxAmnt(i+1,robTill,nums,dp);
+        int rob= nums[i]+robMaxAmnt(i+2,robTill,nums,dp);
+        dp[i]= Math.max(rob,skip);
+
+        return dp[i];
+
+    }
+}
