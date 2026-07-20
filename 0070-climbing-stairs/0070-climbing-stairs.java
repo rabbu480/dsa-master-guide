28868220eee4bdfd05693d063da047e4627df5ca
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-07-20 12:49:42 +0530

    Time: 0 ms (100%), Space: 42 MB (72.82%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -3,7 +3,7 @@ class Solution {
         // 4: memorization 
         int[] dp= new int[n+1];
         Arrays.fill(dp, -1);
-        System.out.println(" dp[i]::::"+ Arrays.toString(dp));
+        // System.out.println(" dp[i]::::"+ Arrays.toString(dp));
         return ways(0,n,dp);
     }
 
@@ -15,9 +15,11 @@ class Solution {
         if(dp[i] != -1 ){
             return dp[i];
         }
-        System.out.println(" dp[i]>>>::::"+ Arrays.toString( dp));
+        // System.out.println(" dp[i]>>>::::"+i+"---"+ Arrays.toString( dp));
+        // complete path after ith step  can tak 1 or 2 like wise each time we checking 
         dp[i]=ways(i+1,n,dp)+ways(i+2,n,dp);
-        System.out.println(" dp[i]>2>>::::"+ Arrays.toString( dp));
+        // System.out.println(" dp[i]>>>::::"+i+"---"+ Arrays.toString( dp));
+        // System.out.println(" dp[i]>>>::::"+i+"---"+ dp[i]);
         return dp[i];
     }
 }
