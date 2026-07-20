9a5d5f7c2fe409984904e4b29e9a96c5f338630f
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-07-20 12:18:11 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.64%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
+        // 4: memorization 
         int[] dp= new int[n+1];
         Arrays.fill(dp, -1);
         return ways(0,n,dp);
     }
 
+    // state & choice ::  i th position , +1 & +2 
+    // Recurrence & Basecase & recursion:: 
     int ways(int i,int n,int[] dp){
-
-        // state & choice ::  i th position , +1 & +2 
-        // Recurrence & Basecase & recursion:: 
         if(i == n) return 1;
         if(i > n ) return 0;
         if(dp[i] != -1 ){
