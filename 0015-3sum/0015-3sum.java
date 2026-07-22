9bc3b995a05480716b3190f008ca26788b449d92
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-07-22 10:30:33 +0530

    Time: 29 ms (93.71%), Space: 59.4 MB (24.56%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,39 +1,17 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
-        // Set<List<Integer>> resultSet= new HashSet<>();
-        
-        // for(int i =0 ; i< nums.length ; i++){
-        //     Set<Integer> set = new HashSet<>();
-        //     for(int j =i+1 ; j< nums.length ; j++){
-        //         if(set.contains(-nums[i] - nums[j])){
-        //             resultSet.add(Arrays.asList(-nums[i]-nums[j],nums[i],nums[j]));
-        //         }
-        //         set.add(nums[j]);
-        //     }
-        // }
-
-        // List<List<Integer>> result=new ArrayList();
-
-        // for(List sums: resultSet){
-        //     result.add(sums);
-        // }
-        
-
-        // return result;
-
-
         // Fix one element till length-2 skip duplicate i 
         // left =i +1 right=n-1 
         // Arrays.sort()
         // sum < target  left++
         // sum > taregt right--
         // sum == target  left++ right --
-            // Arrays.asList(num[i],num[left],num[right]);
-            // eleminate duplicates left < right num[left] == nums[left-1]
-            // eleminate duplicates left < right num[right] == nums[right-1] 
+        //     Arrays.asList(num[i],num[left],num[right]);
+        //     eleminate duplicates left < right num[left] == nums[left-1]
+        //     eleminate duplicates left < right num[right] == nums[right-1] 
+        
         Arrays.sort(nums);
-        System.out.println("nums ->" +Arrays.toString(nums));
         List<List<Integer>> result=new ArrayList<>();
         for(int i=0; i< nums.length-2; i++) {
             int left=i+1;
