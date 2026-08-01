65cce1df4dfe0c0d0b6f24e09a3d59fdaa4b37c3
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-08-01 23:36:25 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,25 +1,33 @@
 class Solution {
     public int climbStairs(int n) {
-        // 4: memorization 
+        // // 4: memorization 
         int[] dp= new int[n+1];
         Arrays.fill(dp, -1);
-        // System.out.println(" dp[i]::::"+ Arrays.toString(dp));
-        return ways(0,n,dp);
+        // // System.out.println(" dp[i]::::"+ Arrays.toString(dp));
+        return ways(n,dp);
+        // return ways(0,n);
+    }
+    int ways(int n,int[] dp){
+        if(n==0) return 1;
+        if(n<0) return 0;
+        if(dp[n] != -1) return dp[n];
+        dp[n]= ways(n-1,dp) +  ways(n-2,dp);
+        return dp[n];
     }
 
     // state & choice ::  i th position , +1 & +2 
     // Recurrence & Basecase & recursion:: 
-    int ways(int i,int n,int[] dp){
-        if(i == n) return 1;
-        if(i > n ) return 0;
-        if(dp[i] != -1 ){
-            return dp[i];
-        }
-        // System.out.println(" dp[i]>>>::::"+i+"---"+ Arrays.toString( dp));
-        // complete path after ith step  can tak 1 or 2 like wise each time we checking 
-        dp[i]=ways(i+1,n,dp)+ways(i+2,n,dp);
-        // System.out.println(" dp[i]>>>::::"+i+"---"+ Arrays.toString( dp));
-        // System.out.println(" dp[i]>>>::::"+i+"---"+ dp[i]);
-        return dp[i];
-    }
+    // int ways(int i,int n,int[] dp){
+    //     if(i == n) return 1;
+    //     if(i > n ) return 0;
+    //     if(dp[i] != -1 ){
+    //         return dp[i];
+    //     }
+    //     // System.out.println(" dp[i]>>>::::"+i+"---"+ Arrays.toString( dp));
+    //     // complete path after ith step  can tak 1 or 2 like wise each time we checking 
+    //     dp[i]=ways(i+1,n,dp)+ways(i+2,n,dp);
+    //     // System.out.println(" dp[i]>>>::::"+i+"---"+ Arrays.toString( dp));
+    //     // System.out.println(" dp[i]>>>::::"+i+"---"+ dp[i]);
+    //     return dp[i];
+    // }
 }
