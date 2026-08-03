fcf8399c8d352b750a351e9744da889e69f17771
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-08-04 01:26:44 +0530

    Time: 3 ms (98.22%), Space: 47.2 MB (53.62%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int[][] dp=new int[word1.length()][word2.length()];
+        for(int[] col:dp){
+            Arrays.fill(col,-1);
+        }
+        return Solve(0,0,word1,word2,dp);
+    }
+    // State i, j in word1 & word2 
+    // Choice : insert: Solve(i+1,j+1,word1,word2) Delete : Solve(i+1,j,word1,word2)  Replace:Solve(i,j+1,word1,word2)
+    // recursion : Solve(i+1,j+1,word1,word2) Delete : Solve(i+1,j,word1,word2)  Replace:Solve(i,j+1,word1,word2)
+    // basecase : if(i==word1.length() || j==word2.length()  ) return 0;
+
+
+    int Solve(int i, int j , String word1, String word2,int[][] dp ){
+        if(i == word1.length())
+            return word2.length() - j;
+
+        if(j == word2.length())
+            return word1.length() - i;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(word1.charAt(i) == word2.charAt(j)){
+            dp[i][j]= Solve(i+1, j+1, word1, word2,dp);
+            return dp[i][j];
+        }
+
+        int insert = Solve(i, j+1, word1, word2,dp);
+
+        int delete = Solve(i+1, j, word1, word2,dp);
+
+        int replace = Solve(i+1, j+1, word1, word2,dp);
+
+        dp[i][j]=1+Math.min(insert,Math.min(delete,replace));
+
+        return dp[i][j];
+    }
+}
