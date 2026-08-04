027286811cfa24727a9965b94507b3e9bac63359
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-08-04 22:54:04 +0530

    Time: 116 ms (36.3%), Space: 47.6 MB (5.82%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,6 +1,14 @@
 class Solution {
+
     public int findTargetSumWays(int[] nums, int target) {
-        return Solve(0,nums,target);
+        // int sum=0;
+        // for(int num:nums){
+        //     sum+=num;
+        // }
+        // offset - to + is index so  
+        // int[][] dp=new int[nums.length][target+sum];
+        Map<String,Integer> dp= new HashMap<>();    
+        return Solve(0,nums,target,dp);
     }
     // S at i, Choice : take (+) or skip(-)
     // recurrence: Solve(i+1,nums,remaingTarget-nums[i]) Solve(i+1,nums,remaingTarget)
@@ -8,14 +16,25 @@ class Solution {
     //
 
 
-    int Solve(int i,int[] nums,int remaingTarget){
+    int Solve(int i,int[] nums,int remaingTarget,Map<String,Integer> dp){
 
-        if(i==nums.length) return remaingTarget == 0 ? 1 : 0;
+        if(i==nums.length) {
+            if(remaingTarget == 0) return 1;
+            return 0;
+        } 
 
-        int plus= Solve(i+1,nums,remaingTarget+nums[i]);
-        int minus= Solve(i+1,nums,remaingTarget-nums[i]);
+        String key= "#"+i+"-"+remaingTarget+"#";
 
-        return plus+minus;
+        if(dp.containsKey(key)){
+          return  dp.get(key);
+        }
+
+        int plus= Solve(i+1,nums,remaingTarget+nums[i],dp);
+        int minus= Solve(i+1,nums,remaingTarget-nums[i],dp);
+        int result=plus+minus;
+        dp.put(key, result);
+
+        return dp.get(key) ;
     }
     
 }
