fb1b0131e9fc8e5e99792a1bec5136e480b99d88
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-08-10 08:36:52 +0530

    Time: 2 ms (72.63%), Space: 47.4 MB (93.67%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -29,14 +29,10 @@ class Solution {
         for(int i=0;i<m; i++){
             for(int j =0; j<n ; j++){
                 if(board[i][j] == 'O') board[i][j] = 'X';
-            }
-            for(int j =0; j<n ; j++){
-               if(board[i][j] == 'S') board[i][j] = 'O';
+                if(board[i][j] == 'S') board[i][j] = 'O';
             }
         }
-        // for(int i=0;i<m; i++){
-            
-        // }
+
 
         
     }
