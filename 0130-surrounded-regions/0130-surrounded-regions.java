70c0972911af68ac406eb225427e78167d1e24cf
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-08-10 08:36:11 +0530

    Time: 2 ms (72.63%), Space: 47.6 MB (88.17%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -14,30 +14,29 @@ class Solution {
         int n = board[0].length; //cols j n
 
         for(int j =0; j<n ; j++){
-            if(board[0][j] == 'O'){
-                dfs(0,j,board);
-            }
-        }
-        for(int j =0; j<n ; j++){
+            if(board[0][j] == 'O') {    dfs(0,j,board);}
             if(board[m-1][j] == 'O') dfs(m-1,j,board); 
         }
+        // for(int j =0; j<n ; j++){
+        //     if(board[m-1][j] == 'O') dfs(m-1,j,board); 
+        // }
         for(int i =0; i<m ; i++){
             if(board[i][0] == 'O') dfs(i,0,board);
-        }
-        for(int i =0; i<m ; i++){
             if(board[i][n-1] == 'O') dfs(i,n-1,board);
         }
+
 
         for(int i=0;i<m; i++){
             for(int j =0; j<n ; j++){
                 if(board[i][j] == 'O') board[i][j] = 'X';
             }
-        }
-        for(int i=0;i<m; i++){
             for(int j =0; j<n ; j++){
                if(board[i][j] == 'S') board[i][j] = 'O';
             }
         }
+        // for(int i=0;i<m; i++){
+            
+        // }
 
         
     }
