9ff6a5a1bb16b2e36c90d43f2ac3bd18fff63665
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-09-14 17:43:44 +0530

    Time: 29 ms (0.94%), Space: 52.5 MB (6.95%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -13,23 +13,62 @@ class Solution {
     // recursion 
     // remove
 
-    void solve(int[] nums,int i,List<Integer> path){
+    // void solve(int[] nums,int i,List<Integer> path){
 
-        if(i == nums.length){
-            System.out.println("path :::>> "+ path);
-            result.add(new ArrayList(path));
-            return;
-        }
-               //skip:
-        System.out.println("SKIP >>" +nums[i]);
-        solve(nums,i+1,path);
-        
-        // take : 
-        path.add(nums[i]);
-        System.out.println("takE >>" +nums[i]);
-        solve(nums,i+1,path);
-        path.remove(path.size()-1); 
+    //     if(i == nums.length){
+    //         System.out.println("path :::>> "+ path);
+    //         result.add(new ArrayList(path));
+    //         return;
+    //     }
+
+    //     // take : 
+    //     path.add(nums[i]);
+    //     System.out.println("takE >>" +nums[i]);
+    //     solve(nums,i+1,path);
+    //     path.remove(path.size()-1); 
+
+    //     //skip:
+    //     System.out.println("SKIP >>" +nums[i]);
+    //     solve(nums,i+1,path);
         
  
+    // }
+
+    void solve(int[] nums, int i, List<Integer> path) {
+
+        System.out.println("ENTER  i=" + i + " path=" + path);
+
+        if (i == nums.length) {
+            System.out.println("BASE   i=" + i + " path=" + path);
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+    // TAKE
+        path.add(nums[i]);
+        System.out.println("TAKE   " + nums[i] + " → " + path);
+
+        solve(nums, i + 1, path);
+
+        // UNDO
+        path.remove(path.size() - 1);
+        System.out.println("UNDO   " + nums[i] + " → " + path);
+
+        // SKIP
+        System.out.println("SKIP   " + nums[i] + " → " + path);
+
+        solve(nums, i + 1, path);
     }
+
+    // void solve(int[] nums, int i, List<Integer> path) {
+    //     result.add(new ArrayList<>(path));
+    //     // choose one each combinations
+    //     for(int j = i ; j< nums.length ; j++ ){
+    //         path.add(nums[j]);
+    //         System.out.println("TAKE   " + nums[j] + " → " + path);
+    //         solve(nums, j + 1, path);
+    //         path.remove(path.size() - 1);
+    //     }
+
+    // }
 }
