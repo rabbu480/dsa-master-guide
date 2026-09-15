9060d56dfcc50d1d38d8b7c5eda7214a623a88a1
Rabbani.Shaik <53053185+rabbu480@users.noreply.github.com>
2026-09-15 10:04:20 +0530

    Time: 1 ms (53.37%), Space: 43.3 MB (89.23%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,46 @@
+class Solution {
+
+    List<String> result= new ArrayList<>();
+    Map<String,String> digiMap= new HashMap<>();
+
+
+    public List<String> letterCombinations(String digits) {
+
+        digiMap.put("2","abc");
+        digiMap.put("3","def");
+        digiMap.put("4","ghi");
+        digiMap.put("5","jkl");
+        digiMap.put("6","mno");
+        digiMap.put("7","pqrs");
+        digiMap.put("8","tuv");
+        digiMap.put("9","wxyz");
+
+        Solve(0,digits,new StringBuffer(""));
+        return result;
+        
+    }
+
+    void Solve(int i,String digits,StringBuffer path) {
+
+        if(path.length() == digits.length()){
+            result.add(""+path);
+            return;
+        }
+
+        // char[] digiChar=digits.toCharArray();
+        for(int j =i ; j < digits.length() ; j++){
+            String letter =digiMap.get(""+digits.charAt(j));
+            for(char c: letter.toCharArray()) {  
+                path.append(c);
+                Solve(j+1,digits,path);
+                path.deleteCharAt(path.length() - 1);
+            }
+        }
+
+    }
+
+
+
+
+
+}
